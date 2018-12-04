f9b9072ff9177b9e295a0ae06bd1ad6886ac39c6
Luis Enrique Vargas Sánchez <luisenrique@MacBook-Pro-de-Luis.local>
2018-12-04 00:35:10 -0600

    final commit

--- fake-bot.js
+++ fake-bot.js
@@ -1,31 +1,79 @@
+var image = document.querySelector(".bot");
 var hello = document.querySelector(".sayHi");
 var descripcion = document.querySelector(".descripcion");
 var hobby = document.querySelector(".hobby");
 var inName = document.querySelector(".inName");
-// var validName = document.querySelector(.inName.value)
-
+var longName = document.querySelector(".longName");
+var shortName = document.querySelector(".shortName");
+var hi = document.querySelector(".hi");
+var sayHello = document.querySelector(".nombre");
 
 setTimeout(function(){
-  hello.style.display = "block";
+  image.style.display = "block";
 },1000);
 
 setTimeout(function(){
-  descripcion.style.display = "block";
+  hello.style.display = "block";
 },2000);
 
 setTimeout(function(){
-  hobby.style.display = "block";
+  descripcion.style.display = "block";
 },3000);
 
 setTimeout(function(){
-  inName.style.display = "block";
+  hobby.style.display = "block";
 },4000);
 
-document.getElementById("#validate-field").addEventListener('keypress',function(e){
+setTimeout(function(){
+  inName.style.display = "block";
+},5000);
 
-  if (e.keyCode == 13);
-  {
-    alert (false);
+
+inName.addEventListener("keypress", function(e){
+
+  if (e.keyCode === 13){
+
+    if(inName.value === "" || inName.value.length < 2){
+
+      alert("Write your name.")
+
+    } else if (inName.value.length >= 2){
+
+      if(inName.value.length > 6){
+
+        setTimeout(function() {
+
+          longName.style.display = "block"
+
+          shortName.style.display = "none"
+
+        },1000)
+
+        setTimeout(function() {
+
+          hi.style.display = "block"
+
+          sayHello.textContent = inName.value
+        },2000)
+      }
+
+      if(inName.value.length <= 6){
+
+        setTimeout(function() {
+
+          shortName.style.display = "block"
+
+          longName.style.display = "none"
+        },1000)
+
+        setTimeout(function() {
+
+          hi.style.display = "block"
+
+          sayHello.textContent = inName.value
+
+        },2000)
+      }
+    }
   }
-
-});
+})
